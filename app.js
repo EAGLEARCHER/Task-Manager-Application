73bd5a1da5de4612d7e167b7d9f359e4f318ef4e
EAGLEARCHER <bherusinghpanwar43@gmail.com>
2023-12-04 23:49:40 +0530

    asyncWrapper for controllers and some frontend fixes

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 const express = require("express");
 require("dotenv").config();
-const taskRoutes = require('./routes/taskRoutes');
-const database = require('./db/connect');
+const taskRoutes = require("./routes/taskRoutes");
+const database = require("./db/connect");
 const port = process.env.PORT;
+const uri = process.env.MONGO_URI;
 const app = express();
 // middleware
 
-app.use(express.static('./public'));
+app.use(express.static("./public"));
 app.use(express.json());
 
+app.use("/api/tasks", taskRoutes);
+const start = async () => {
+  try {
+    await database(uri)
+      .then(() => {
+        console.log("database started...");
+      })
+      .catch((err) => {
+        console.log(`error ${err}`);
+      });
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-app.use("/tasks", taskRoutes)
-
-app.listen(port,()=>{
-    console.log(`Server is running on ${port}`);
-});
+start();
